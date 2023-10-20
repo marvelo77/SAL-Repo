52c80d961e71803622926bdb7f97dd8e0a791238
Kri5 <marvelo77@gmail.com>
2023-10-20 11:33:23 -0300

    createTables

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,26 @@
 const mysql = require('mysql2');
 
-const mysqlConnection = mysql.createConnection({
+//mysql pingu
+/*const mysqlConnection = mysql.createConnection({
     host: '10.11.33.8',
+    user: 'saldbdapp',
+    password: 'carpo',
+    database: 'SALDBD'
+
+});
+
+mysqlConnection.connect(function (err){
+    if(err){
+        console.log(err);
+        return;
+    } else {
+        console.log('Db is connected');
+    }
+});*/
+
+//mysql tcom
+const mysqlConnection = mysql.createConnection({
+    host: '10.76.20.51',
     user: 'saldbdapp',
     password: 'carpo',
     database: 'SALDBD'
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -5,8 +5,11 @@ const mysqlConnection = require('../database/database');
 
 //const { rows } = require('pg/lib/defaults');
 
+//const order_select = 'select o.*,poi.ProductorderitemID,poi.Action,poi.Status,pr.Code,pr.Name,pr.Price,pr.Tax,pr.Validfor_startdatetime, pr.Validfor_enddatetime from saldbd.`order` o inner join saldbd.`Productorderitem` poi on o.OrderID = poi.OrderID inner join saldbd.`productofferingprice` pr on pr.ProductorderitemID = poi.ProductorderitemID';
+const order_select = 'SELECT * FROM `Order`';
+
 router.get('/order', (req, res) => {
-    mysqlConnection.query('select o.*,poi.ProductorderitemID,poi.Action,poi.Status,pr.Code,pr.Name,pr.Price,pr.Tax,pr.Validfor_startdatetime, pr.Validfor_enddatetime from saldbd.order o inner join saldbd.Productorderitem poi on o.OrderID = poi.OrderID inner join saldbd.productofferingprice pr on pr.ProductorderitemID = poi.ProductorderitemID', (err,rows,fields) => {
+    mysqlConnection.query(order_select, (err,rows,fields) => {
         if (!err) {
             res.json(rows);
         } else {
